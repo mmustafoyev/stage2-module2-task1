d888f91b43d013f7c50781f9d21b5a39d0b98dda
mmustafoyev <135558935+mmustafoyev@users.noreply.github.com>
2023-08-12 16:21:59 +0500

    Update GetUsersServlet.java

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,26 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet{
     //write your code here!
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Warehouse w = Warehouse.getInstance();
+        Set<User> users = w.getUsers();
+        req.setAttribute("users", users);
+
+        resp.sendRedirect("jsp/users.jsp");
+    }
 }
