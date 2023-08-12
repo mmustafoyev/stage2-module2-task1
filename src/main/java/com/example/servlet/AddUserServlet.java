3f51580cb1578d685c749e15d787fe4a4453ea66
mmustafoyev <135558935+mmustafoyev@users.noreply.github.com>
2023-08-12 16:21:19 +0500

    Update AddUserServlet.java

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,37 @@
 package com.example.servlet;
 
-public class AddUserServlet {
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
     //write your code here!
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.sendRedirect("jsp/add.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("came to doPost method");
+        String firstName = req.getParameter("firstName");
+        String secondName = req.getParameter("lastName");
+        User user = new User(firstName, secondName);
+        Warehouse warehouse = Warehouse.getInstance();
+        warehouse.addUser(user);
+        req.setAttribute("user", user);
+
+
+        req.getRequestDispatcher("jsp/users.jsp").forward(req, resp);
+    }
 }
